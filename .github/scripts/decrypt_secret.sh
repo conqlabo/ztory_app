532b6c9a23a213b3fcbcdbe0794bdc202694a3c1
paulpascal <paul.alogno@gmail.com>
2023-02-04 04:28:19 +0000

    feat: always notify on slack

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,5 +4,5 @@
 gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_KEYS_ZIP_PASSPHRASE" \
 --output android/key_files.zip android/key_files.zip.gpg && cd android && jar xvf key_files.zip && cd -
 ls -d $PWD/android/*
-mv ./android/keystore.jks ./android/app
+# mv ./android/keystore.jks ./android/app
 # move your file according to path in key.properties
